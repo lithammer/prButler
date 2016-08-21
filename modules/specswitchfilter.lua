f34274ccc65f5a3f12ac627c5be8fb32b3157b06
Peter Renström <renstrom.peter@gmail.com>
2016-08-21 15:52:54 +0200

    specswitchfilter: Update spell ID used for spec change

--- modules/specswitchfilter.lua
+++ modules/specswitchfilter.lua
@@ -7,9 +7,6 @@ local spamFilterMatch2 = string.gsub(ERR_LEARN_SPELL_S:gsub('%.', '%.'), '%%s', 
 local spamFilterMatch3 = string.gsub(ERR_SPELL_UNLEARNED_S:gsub('%.', '%.'), '%%s', '(.*)')
 local spamFilterMatch4 = string.gsub(ERR_LEARN_PASSIVE_S:gsub('%.', '%.'), '%%s', '(.*)')
 
-local primarySpecSpellName = GetSpellInfo(63645)
-local secondarySpecSpellName = GetSpellInfo(63644)
-
 local HideSpam = CreateFrame('Frame')
 HideSpam:RegisterEvent('PLAYER_LOGIN')
 HideSpam:RegisterUnitEvent('UNIT_SPELLCAST_START', 'player')
@@ -30,25 +27,23 @@ HideSpam.filter = function(self, event, msg, ...)
 end
 
 HideSpam:SetScript('OnEvent', function(self, event, ...)
-	local unit, spellName = ...
+	local spellId = select(5, ...)
+	local activateSpecSpellId = 200749
 
 	if event == 'PLAYER_LOGIN' then
 		HideSpam:RegisterEvent('ACTIVE_TALENT_GROUP_CHANGED')
 		HideSpam:UnregisterEvent('PLAYER_LOGIN')
 	elseif event == 'UNIT_SPELLCAST_START' then
-		if unit == 'player' and (spellName == primarySpecSpellName or spellName == secondarySpecSpellName) then
+		if spellId == activateSpecSpellId then
 			ChatFrame_AddMessageEventFilter('CHAT_MSG_SYSTEM', self.filter)
 		end
-	
 	elseif event == 'UNIT_SPELLCAST_STOP' or event == 'UNIT_SPELLCAST_INTERRUPTED' then
-		if unit == 'player' and (spellName == primarySpecSpellName or spellName == secondarySpecSpellName) then
+		if spellId == activateSpecSpellId then
 			ChatFrame_RemoveMessageEventFilter('CHAT_MSG_SYSTEM', self.filter)
 		end
-	
 	elseif event == 'ACTIVE_TALENT_GROUP_CHANGED' then
 		if GetSpecialization() then
-			local id, name, description, icon, background, role = GetSpecializationInfo(GetSpecialization())
-
+			local _, name, _, icon = GetSpecializationInfo(GetSpecialization())
 			local text = 'Switched to |T'..icon..':0|t |cff6adb54'..name..'|r specialization.'
 
 			DEFAULT_CHAT_FRAME:AddMessage(text, 255, 255, 255)
